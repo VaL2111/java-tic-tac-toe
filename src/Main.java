d80fb6730c16fd6ed19c1c2e817c14bbec23c7b8
Valentyn <143123781+VaL2111@users.noreply.github.com>
2025-09-29 21:43:05 +0300

    implement coordinate imput with validation

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
 
     private static byte ROW_COUNT = 3;
@@ -7,9 +10,13 @@ public class Main {
     private static String CELL_STATE_X = "X";
     private static String CELL_STATE_O = "O";
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String[][] board = createBoard();
+        board[0][0] = CELL_STATE_X;
+
+        int[] ints = inputBoardCoordinates(board);
     }
 
     public static String[][] createBoard() {
@@ -22,5 +29,24 @@ public class Main {
         }
 
         return board;
+    }
+
+    public static int[] inputBoardCoordinates(String[][] board) {
+        System.out.print("Введіть дві цифри від 0 до 2 через пробіл: ");
+
+        do {
+            String[] input = scanner.nextLine().split(" ");
+
+            int row = Integer.parseInt(input[0]);
+            int col = Integer.parseInt(input[1]);
+
+            if ((row < 0) || (row >= ROW_COUNT) || (col < 0) || (col >= COL_COUNT)) {
+                System.out.print("Неправильний ввід! Введіть дві цифри від 0 до 2 через пробіл: ");
+            } else if (!Objects.equals(board[row][col], CELL_STATE_EMPTY)) {
+                System.out.print("Ця комірка вже зайнята. Виберіть іншу: ");
+            } else {
+                return new int[]{row, col};
+            }
+        } while (true);
     }
 }
