29b680ec6e7eea827fc1c427ba6498dc6ac9784b
Valentyn <143123781+VaL2111@users.noreply.github.com>
2025-09-29 21:05:30 +0300

    feat: add game constants and board creation logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+
+    private static byte ROW_COUNT = 3;
+    private static byte COL_COUNT = 3;
+
+    private static String CELL_STATE_EMPTY = " ";
+    private static String CELL_STATE_X = "X";
+    private static String CELL_STATE_O = "O";
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+
+    public static String[][] createBoard() {
+        String[][] board = new String[ROW_COUNT][COL_COUNT];
+
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COL_COUNT; col++) {
+                board[row][col] = CELL_STATE_EMPTY;
+            }
+        }
+
+        return board;
     }
 }
